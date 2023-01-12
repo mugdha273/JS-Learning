6b6958559a5d3d3f083715be4691f3efd9752991
Mugdha Sharma <85048574+mugdha273@users.noreply.github.com>
2023-01-12 09:43:17 +0000

    className and classList

---
+++ JS_45_classname_classlist/script.js
@@ -0,0 +1,7 @@
+first.className = "text-dark"
+// first.classList.add("yellow")
+// first.classList.remove("red")
+// first.classList.contains("red")
+
+//toggle-> lga hai toh hta do, hta hua hai toh lga dos
+first.classList.toggle("red")
