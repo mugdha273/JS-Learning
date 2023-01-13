5ee30e928b8222f9c6501982ea97e1d9360b3d30
mugdha273 <mugdhasharma0327@gmail.com>
2023-01-13 11:41:31 +0530

    browserEvents

---
+++ JS_48_handling_browserEvents/script.js
@@ -0,0 +1,30 @@
+btn = document.getElementById("btn")
+
+//these happens when one process is completed, although you can ask for another process to be done by stopping the first one to execute
+// btn.addEventListener('click',function(){
+//     alert("you are into a click event")
+// })
+
+// btn.addEventListener('click',function(){
+//     alert("you are into a click event2")
+// })
+
+
+//if you want to remove the event listener
+
+let x= function(){
+    alert("you are into a click event1")
+}
+
+let y= function(){
+    alert("you are into a click event2")
+}
+
+btn.addEventListener('click',x)
+btn.addEventListener('click',y)
+
+let a = prompt("do you want to remove the event listener?")
+
+if(a=="yes"){
+    btn.removeEventListener('click',x)
+}
