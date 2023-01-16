8ec4481331f1d6418087776f5b2d62c424bbf175
mugdha273 <mugdhasharma0327@gmail.com>
2023-01-16 14:33:19 +0530

    practice set 8

---
+++ JS_PracticeSet_chapter8/script.js
@@ -0,0 +1,21 @@
+//question 3
+/* let b = document.getElementById("twitter");
+ b.addEventListener("click", function () {
+     let site = "https://twitter.com/";
+     window.open(site, "_blank");
+ }); */
+
+//question 4
+
+// setInterval(function () {
+
+//     let url = 'https://jsonplaceholder.typicode.com/todos/1'
+//     console.log(fetch(url))
+// }, 5000);
+
+//question 5
+// setInterval(function () {
+
+// document.querySelector("#bulb").classList.toggle("bulb");
+
+// }, 1000);
